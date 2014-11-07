2101d6bee9ac4604a2e65c08c7aa6858593655b1
Phil Martel <pmartel@alum.mit.edu>
2014-11-06 21:31:33 -0500

    working on row, column and box values
    next add a Cell array with to SudokuGame

--- SudokuGame.py
+++ SudokuGame.py
@@ -2,12 +2,50 @@
 
 from tkinter import *
 
+class Cell:
+    row = []
+    col = []
+    box = []
+    val = []
+    origVal = []
+    
+    def __init__(self, r, c, box, val):
+        self.row = r
+        self.col = c
+        self.box = box
+        self.val = val
+        self.OrigVal = val
+
+    def reset(self):
+        self.val = self.origVal
+
+    def get(self):
+        return self.val
+
+    def set(self,v):  # note: this overloads the builtin class set
+        if self.val == [] :
+            self.val = v
+        else:
+            # some warning if you try to load over a non-blank?
+            pass
+
+    
 class SudokuGame:
     def __init__(self, tk, n=3):
         """ tk is the tkinter object that will display the board.
             n is a number that specifies the board size n^2 x n^2
             Normally 3, it could be 4 (use hex digits).  Possibly 2 for
             a kid. """
+        self.tk = tk
+        self.n = n
+        self.nSq = n * n
+        self.numCells = self.nSq * self.nSq
+        for k in range(self.numCells):
+            ( r, c) = divmod(k, self.nSq)
+            rr = r // self.n
+            cc = c // self.n
+            b = rr * self.n + cc
+            print(k,r,c,b)
         pass
 
     def load(self, fileName):
@@ -25,7 +63,7 @@ class SudokuGame:
     def check(self):
         pass
 
-    def backup(self):
+    def undo(self): # was called backup
         pass
   
 # main routine
@@ -33,7 +71,7 @@ class SudokuGame:
 root = Tk()
 
 # start the game
-game = SudokuGame(root)
+game = SudokuGame(root,2)
 #game.mainloop()
 # for debug
 
