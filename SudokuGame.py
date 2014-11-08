36b12b1d7c07fe5b6f2f9af88c94c1d55841f1c0
Phil Martel <pmartel@alum.mit.edu>
2014-11-07 21:36:53 -0500

    grid displays as buttons
    numbers don't display yet

--- SudokuGame.py
+++ SudokuGame.py
@@ -9,34 +9,62 @@ class Cell:
     val = []
     origVal = []
     
-    def __init__(self, r, c, box, val):
+    def __init__(self, r, c, box, tk, size):
         self.row = r
         self.col = c
         self.box = box
-        self.val = val
-        self.OrigVal = val
+        self.tk = tk
+        self.size=size
+        if size == 16:
+            self.base = 16
+        else:
+            self.base = 10
+        # set up value and button
+        self.val = ' '
+        self.OrigVal = ' '
+        self.but = Button(tk, bg='white', command = self.pressed,\
+                          padx=10,pady=10,textvariable=self.val)
+        self.but.grid(row=r+1, column=c+1)
 
     def reset(self):
         self.val = self.origVal
 
-    def get(self):
+    def getv(self):
         return self.val
 
-    def set(self,v):  # note: this overloads the builtin class set
-        if self.val == [] :
-            self.val = v
+    def setv(self,v):  # note: just set() overloads the builtin class set
+        if self.val == ' ':
+            self.val = str(v)
         else:
             # some warning if you try to load over a non-blank?
             pass
 
+        
+    def pressed(self):
+        v = input('? ')
+        vInt = int(v,self.base)
+        if vInt >= 1 and vInt <= self.size :
+            self.setv(vInt)
+        else:
+            print('Bad value. Should be digit between 1 and',self.size)
+        pass
     
-class SudokuGame:
+class SudokuGame(Frame):
+    cell=[]
     def __init__(self, tk, n=3):
         """ tk is the tkinter object that will display the board.
             n is a number that specifies the board size n^2 x n^2
             Normally 3, it could be 4 (use hex digits).  Possibly 2 for
             a kid. """
+        # set up the graphics
         self.tk = tk
+        tk.title('Sudoku - Phil Martel')
+        tk.geometry('500x500')
+        super(SudokuGame,self).__init__(tk)
+
+        # set up the cells
+        lab1 = Label(tk,text='  ')
+        lab1.grid(row=0,column=0)
         self.n = n
         self.nSq = n * n
         self.numCells = self.nSq * self.nSq
@@ -45,7 +73,9 @@ class SudokuGame:
             rr = r // self.n
             cc = c // self.n
             b = rr * self.n + cc
-            print(k,r,c,b)
+            # this checks that r,c, and b are good
+            #print(k,r,c,b)
+            self.cell.append(Cell(r,c,b,tk,self.nSq))
         pass
 
     def load(self, fileName):
@@ -71,7 +101,7 @@ class SudokuGame:
 root = Tk()
 
 # start the game
-game = SudokuGame(root,2)
+game = SudokuGame(root)
 #game.mainloop()
 # for debug
 
