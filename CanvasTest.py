02f7f777ae4d2207dc92f9b2ae7dde5676823220
Phil Martel <pmartel@alum.mit.edu>
2014-11-14 21:37:46 -0500

    got lines working and a bell sound

--- CanvasTest.py
+++ CanvasTest.py
@@ -6,4 +6,11 @@ test.title( "Canvas Test")
 test.geometry('500x500')
 can = Canvas(test,height=300, width=300, bg='green')
 can.grid(row=1,column=1)
+n = 0
+hLin = []
+vLin = []
+for x in range(1,6):
+    hLin.append(can.create_line(0,x*50,500,x*50,fill='blue'))
+    vLin.append(can.create_line(x*50,0,x*50,500,fill='red'))
 
+can.bell()
