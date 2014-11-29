28a192dba29961aa784199784232bac370e51d63
Phil Martel <pmartel@alum.mit.edu>
2014-11-28 20:40:41 -0500

    CanvasTest puts together a board
    with entry boxes on the canvas and lines for separation.  The concept
    should be moved to SudokuGame

--- CanvasTest.py
+++ CanvasTest.py
@@ -1,17 +1,50 @@
 # test of using Canvas with buttons
 from tkinter import *
+from time import *
 
 test = Tk()
 test.title( "Canvas Test")
-test.geometry('500x500')
-can = Canvas(test,height=300, width=300, bg='green')
+siz = 50
+xo=3
+yo=xo
+gs='{0}x{1}'.format(siz*10,siz*10)
+test.geometry(gs)
+n=3
+n2 = n*n
+
+can = Canvas(test,height=n2*siz+yo, width=n2*50+xo, bg='light gray')
 can.grid(row=1,column=1)
-n = 0
+
+winList =[]
+entList=[]
 hLin = []
 vLin = []
-for x in range(1,6):
-    hLin.append(can.create_line(0,x*50,500,x*50,fill='blue'))
-    vLin.append(can.create_line(x*50,0,x*50,500,fill='red'))
+#draw outline
+for x in range(0,n2+1):
+    if x % 3 == 0:
+        wid = 3
+    else:
+        wid = 1
+    hLin.append(can.create_line(0,x*siz+yo,500,x*siz+yo,fill='black',width=wid))
+    vLin.append(can.create_line(x*siz+xo,0,x*siz+xo,500,fill='black',width=wid))
 
-can.bell()
+for r in range(n2):
+    for c in range(n2):
+        sv = StringVar()
+        ent = Entry(can,justify=CENTER, width=1,font=('Arial',16),\
+                    textvariable=sv)
+        entList.append(ent)
+        w = can.create_window(c*siz+2*xo,r*siz+2*yo,anchor = NW,\
+                              height = siz-2*xo, width=siz-2*yo,window=ent)
+        winList.append(w)
+
+        
+#can.bell()
 #t = Toplevel(height=100,width=100)
+
+for n in range(81):
+    e = entList[n]
+    v=e['text']
+    e.setvar(v,str(n+1))
+
+
