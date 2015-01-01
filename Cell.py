dbae893f9ee5b5b384f995d9c7cdb065c576ddc0
Phil Martel <pmartel@alum.mit.edu>
2014-12-31 20:44:18 -0500

    debugging and adding info to isOk()
    I found out that the first time an Entry is touched isOk() is called,
    but once a value has been written, isOk is not called.

--- Cell.py
+++ Cell.py
@@ -14,6 +14,7 @@ class Cell:
         self.row = r
         self.col = c
         self.box = box
+        self.idx = r * game.nSq + c
         self.tk = can
         self.game = game # to reference things at the game level
         self.size=game.nSq
@@ -22,6 +23,8 @@ class Cell:
             self.base = 16
         else:
             self.base = 10
+        self.digits = self.game.digits[self.size]
+
         # set up Entry widget
         siz = game.boxSize
         xo = game.xOff
@@ -29,36 +32,58 @@ class Cell:
         
         sv = StringVar()
         okCmd = can.register(self.isOk)
-        self.ent = Entry(can,justify=CENTER, width=1,font=('Arial',16),
+        self.ent = Entry(can, justify=CENTER, width=1, font=('Arial',16),
                     textvariable=sv, validate='all',
-                    validatecommand=(okCmd,'%P','%s','%V') )
+                    validatecommand=(okCmd,'%d','%i','%P','%s','%S',
+                                     '%v','%V','%W') )
         self.w = can.create_window(c*siz+2*xo,r*siz+2*yo,anchor = NW,
-                              height = siz-2*xo, width=siz-2*yo,window=self.ent)
-        self.digits = self.game.digits[self.size]
+                                   height = siz-2*xo, width=siz-2*yo,
+                                   window=self.ent)
 
+    """ Callback substitution codes:
+        '%d' - Action code: 0 for an attempted deletion, 1 for an attempted insertion, or -1 if the callback was
+    called for focus in, focus out, or a change to the textvariable.
+        '%i' - When the user attempts to insert or delete text, this argument will be the index of the beginning
+    of the insertion or deletion. If the callback was due to focus in, focus out, or a change to the
+    textvariable, the argument will be -1.
+        '%P' - The value that the text will have if the change is allowed.
+        '%s' - The text in the entry before the change.
+        '%S' - If the call was due to an insertion or deletion, this argument will be the text being inserted or
+    deleted.
+        '%v' - The current value of the widget's validate option
+        '%V' - The reason for this callback: one of 'focusin', 'focusout', 'key', or 'forced' if the
+    textvariable was changed
+        '%W' - The name of the widget.
 
-    def isOk(self, c, oldC, reason):
-##        debug data
-        s = 'row {0} col {1} new<{2}> old<{3}> {4}'.format(
-            self.row,self.col,c,oldC, reason)
-        print(s)
-        if reason != 'key':
-            return False
+    Use them all for now, print them for debug
         
-        if len(c) > 2:
-            print( 'c is too long')
-        elif len(c) == 2:
-            # select the new part
-            n = c.find(oldC)
-            v = c[1-n];
-            if v in self.digits:
-                # v is a good new value, but it has to be jammed in
-                setv(v)
-            #whether the new value is good or bad, don't let it be changed to c
-            return False 
-        elif len(c) == 1:
-            return (c in self.digits)
-        return False
+    """
+    def isOk(self, d, i, p, s, S, v, V, W):
+        retVal = False
+##        debug data
+        print( 'isOk called with:')
+        print( 'self',self)
+        print( '  %d',d)
+        print( '  %i',i)
+        print( '  %p',p)
+        print( '  %s',s)
+        print( '  %S',S)
+        print( '  %v',v)
+        print( '  %V',V)
+        print( '  %W',W)
+
+        if V == 'key':
+            if len(S) > 1:
+                print( '%S is too long')
+            else:
+                if S in self.digits:
+                    # S is a good new value, jam it in
+                    print( 'setting', S)
+                    self.setv(S)
+        #whether the new value is good or bad, don't let it be changed
+        # by the routin that called us.
+        print('returning',retVal )
+        return retVal
     
     def reset(self):
         self.val = self.origVal
@@ -67,6 +92,7 @@ class Cell:
         return self.ent.get()
 
     def setv(self,v):  # note: just set() overloads the builtin class set
+        print("in setv for cell[{0}] set '{1}'".format(self.idx,v))
         if v in self.digits:
             e = self.ent # the Entry
             nam = e['text'] # the name of the StringVar
@@ -74,11 +100,11 @@ class Cell:
             return True
         return False
     
-    def pressed(self):
-        v = input('? ')
-        vInt = int(v,self.base)
-        if vInt >= 1 and vInt <= self.size :
-            self.setv(vInt)
-        else:
-            print('Bad value. Should be digit between 1 and',self.size)
-        pass
+##    def pressed(self):
+##        v = input('? ')
+##        vInt = int(v,self.base)
+##        if vInt >= 1 and vInt <= self.size :
+##            self.setv(vInt)
+##        else:
+##            print('Bad value. Should be digit between 1 and',self.size)
+##        pass
